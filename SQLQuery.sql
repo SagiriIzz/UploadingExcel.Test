dc7c21bef27402488bc0eb9be8cc91e216d067d9
Sagiri Izumi <romaty29@gmail.com>
2023-01-19 13:40:33 +0700

    add sql

---
+++ SQLQuery.sql
@@ -0,0 +1,28 @@
+﻿CREATE DATABASE businessdb
+ 
+CREATE TABLE Code
+(
+    id INT PRIMARY KEY NOT NULL, 
+    code NVARCHAR (20) NOT NULL
+)
+
+CREATE TABLE Processes
+(
+  id INT PRIMARY KEY NOT NULL,
+  nameProcess NVARCHAR(20),
+  codeId int,
+  divisionsId int
+)
+CREATE TABLE Divisions 
+(
+id INT PRIMARY KEY NOT NULL,
+division NVARCHAR(20) NOT NULL,
+)
+
+select Divisions.division, Processes.nameProcess from Divisions
+join Processes on Divisions.id = Processes.divisionsId
+
+select Divisions.division, Code.code, Processes.nameProcess from Divisions
+join Processes on Divisions.id = Processes.divisionsId
+join Code on Processes.codeId = Code.id
+where Code.code
